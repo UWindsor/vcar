9da1019f9eb41978927e814ca1b568cd4ea63682
Eric Pickup <pickup@uwindsor.ca>
2020-03-18 17:19:04 -0400

    Remove unnecessary includes in header file

--- src/include/nodes/controller.h
+++ src/include/nodes/controller.h
@@ -1,25 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/ioctl.h>
-#include <net/if.h>
-#include <netinet/in.h>
-
-#include <linux/if_packet.h>
-#include <linux/if_ether.h>
 #include <linux/can.h>
-
-#include "../logger.h"
-
-#include "nodes.h"
-
-/** nodes header */
-#include "door.h"
-#include "pump.h"
 
 struct can_frame *current;
 
