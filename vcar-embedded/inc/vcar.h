7b386d3553dac76ad8527e609ba79d4c91064020
Dilpreet <dschana6@gmail.com>
2020-03-18 17:20:17 -0400

    Linux CAN library

--- vcar-embedded/inc/vcar.h
+++ vcar-embedded/inc/vcar.h
@@ -1,4 +1,24 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+#include <lib.h>
+
 class vcar {
+    int sock;
+    struct ifreq ifr;
+    struct sockaddr_can addr;
+    struct canfd_frame frame;
+    bool enable_canfd = true;
+    int required_mtu;
+
 public:
     vcar();
 };
