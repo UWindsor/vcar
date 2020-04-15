7b924ed0ce90ba6d16db2c49622513377f94496f
moose <mr.rammo@gmail.com>
2020-04-14 21:01:55 -0400

    vcar server barebones

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
+/*
+    Author: Mostapha Rammo
+    
+    use something like `cansend vcar 5A1#48656c6f` to send data onto the
+    can bus. cansend documentation is helpful.
+*/
+
 #include <iostream>
 #include <vcar.h>
+
+#include <linux/can.h>
+
+#include <unistd.h>
+#include <string.h>
 
 int main() {
     
@@ -18,7 +30,7 @@ int main() {
     s = socket(PF_CAN, SOCK_RAW, CAN_RAW);
     if (s < 0) {
         // Socket connection failed
-        std::cerr << "\\ > Test failed! Could not connect to CAN BUS" << std::endl;
+        std::cerr << "> Test failed! Could not connect to CAN BUS" << std::endl;
         return 1;
     }
 
@@ -33,24 +45,26 @@ int main() {
     std::cout << "> Listening on BUS.." << std::endl;
     struct can_frame frame;
     while (1) {
-        nbytes = read(s, &frame, sizeof(struct can_frame));
+        int nbytes = read(s, &frame, sizeof(struct can_frame));
 
         // Error check
         if (nbytes < 0) {
-            std::cerr << "\\ > Failed to read from CAN RAW socket" << std::endl;
+            std::cerr << "> Failed to read from CAN RAW socket" << std::endl;
             continue;
         }
         if (nbytes < sizeof(struct can_frame)) {
-            std::cerr << "\\ > Incomplete CAN frame received" << std::endl;
+            std::cerr << "> Incomplete CAN frame received" << std::endl;
             continue;
         }
 
         /* CAN frame is populated, do what you will */
         
         // Print out CAN frame data
-        std::cout << "\\ > " << strlen(frame.data) << std::endl;
+        std::cout << "RECEIVED: " << strlen((const char*)frame.data) << std::endl;
+        
+        
         for (int i = 0; i < frame.can_dlc; i++) {
-            std::cout << frame.data[i] << " ";
+            std::cout << std::hex << static_cast<int>(frame.data[i]) << " ";
         }
         std::cout << std::endl;
 
