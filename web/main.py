91902a3a2345f36af60ba29515b56c01f7015f15
Eric Pickup <pickup@uwindsor.ca>
2020-04-17 17:13:30 -0400

    Make web app execute system command and return results

--- web/main.py
+++ web/main.py
@@ -1,6 +1,26 @@
 from flask import Flask
+from flask import request
+import ipdb
+import json
+import os
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def hello_world():
-    return 'Hello, World!'
+  frame = request.get_json()['frame']
+  cmd = "cansend vcar " + frame
+
+  exit = os.system(cmd)
+
+  output_body = {
+
+  }
+
+  return json.dumps(
+    {
+      'success': True if exit == 1 else False,
+      'exit_message': exit
+    }
+  )
+
