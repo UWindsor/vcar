6942244d3c93563c784b2945c5917166482a1894
moose <mr.rammo@gmail.com>
2020-04-17 17:59:31 -0400

    add template

--- web/main.py
+++ web/main.py
@@ -1,20 +1,25 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, render_template
 import json
 import os
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def send_can_message():
-  frame = request.get_json()['frame']
-  cmd = "cansend vcar " + frame
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+        
+    frame = request.get_json()['frame']
+    cmd = "cansend vcar " + frame
 
-  exit = os.system(cmd)
+    exit = os.system(cmd)
 
-  return json.dumps(
-    {
-      'success': True if exit == 0 else False,
-      'exit_message': exit
-    }
-  )
+    return json.dumps(
+        {
+            'success': True if exit == 0 else False,
+            'exit_message': exit
+        }
+    )
+
+if __name__ == '__main__':
+    app.run()
