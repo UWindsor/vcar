49cb9b7852c642ac958a5df908b75eda794a2558
Eric Pickup <pickup@uwindsor.ca>
2020-04-17 17:24:43 -0400

    Send can message through system commands

--- web/main.py
+++ web/main.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask import request
+import json
+import os
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=['POST'])
+def send_can_message():
+  frame = request.get_json()['frame']
+  cmd = "cansend vcar " + frame
+
+  exit = os.system(cmd)
+
+  return json.dumps(
+    {
+      'success': True if exit == 0 else False,
+      'exit_message': exit
+    }
+  )
