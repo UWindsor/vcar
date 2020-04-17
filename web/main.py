45f2178d9bed1f047db0e3618da46c83b2dad0c5
Eric Pickup <pickup@uwindsor.ca>
2020-04-17 18:26:39 -0400

    Integrate the home page and form in a flask template

--- web/main.py
+++ web/main.py
@@ -1,21 +1,23 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, render_template
 import json
 import os
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def send_can_message():
-  frame = request.form['frame']
-  cmd = "cansend vcar " + frame
+  if request.method == 'GET':
+    return render_template('index.html')
+  else:
+    frame = request.form['frame']
+    cmd = "cansend vcar " + frame
 
-  exit = os.system(cmd)
+    exit = os.system(cmd)
 
-  return json.dumps(
-    {
-      'success': True if exit == 0 else False,
-      'exit_message': exit
-    }
-  )
+    return json.dumps(
+      {
+        'success': True if exit == 0 else False,
+        'exit_message': exit
+      }
+    )
   
