142009af1cab6b0debf1ee962bd0d2242fa9a57f
Dilpreet <dschana6@gmail.com>
2020-04-19 17:22:11 -0400

    Async connection recv in vcar-embedded

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -r build
-mkdir build
-cd build
-cmake ..
-make
-
-exit
--- vcar-embedded/inc/vcar.h
+++ vcar-embedded/inc/vcar.h
@@ -1,6 +1,12 @@
+#ifndef VCAR_H
+#define VCAR_H
+
 #include <pthread.h>
 #include <cstdlib>
 #include <ctime>
+#include <future>
+#include <list>
+#include <mutex>
 
 #include <net/if.h>
 #include <sys/ioctl.h>
@@ -16,14 +22,35 @@ extern "C" {
 #define VCAR_NAME "vcar"
 
 class vcar {
+    bool running = true;
+
     int sock;
-    struct ifreq ifr;
-    struct sockaddr_can addr;
-    struct canfd_frame frame;
+    struct ifreq ifr {};
+    struct sockaddr_can addr {};
+    struct canfd_frame frame {};
     int enable_canfd = 1;
     int required_mtu;
 
+    std::mutex incoming_connections_mutex;
+    std::list<std::future<int> > incoming_connections;
+
+    std::future<void> vcar_future;
+
+    void start();
+
 public:
     vcar();
+
+    // Disable copying
+    vcar(vcar const&) = delete;
+    vcar& operator=(vcar const&) = delete;
+
+    // Default move
+    vcar(vcar &&) = default;
+    vcar& operator=(vcar &&) = default;
+
     int sendCanFrame(char* sFrame);
+    int recvCanFrame();
 };
+
+#endif
