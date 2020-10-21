6955f24e9649d0afd289d32e4a3c77356f80dc22
Dilpreet Chana <dschana6@gmail.com>
2020-10-21 14:13:56 -0400

    Add rapidjson parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 // TODO: Get the token from REST server to authorize vehicle actions because SQL is a hot garbage
 
+#include <algorithm>
 #include <cstdlib>
 #include <iostream>
+#include <map>
 #include <memory>
+#include <rapidjson/document.h>
 #include <restbed>
+#include <sstream>
 #include <string>
 #include <vcar.h>
-
-#include <linux/can.h>
 
 void PostRegisterUser(std::shared_ptr<restbed::Session> session);
 void PostVehicle(std::shared_ptr<restbed::Session> session);
@@ -16,10 +18,12 @@ void UpdateVehicle(std::shared_ptr<restbed::Session> session);
 void DeleteVehicle(std::shared_ptr<restbed::Session> session);
 
 bool running = true;
+bool default_halted = true;
+std::map<std::string, vcar> vehicles;
 
 int main() {
-    vcar vc;
-    //vc.launch();
+    vehicles.emplace("test", default_halted);
+    //vehicles["test"].launch();
 
     /// REST communication setup
     auto register_user = std::make_shared<restbed::Resource>();
@@ -52,50 +56,116 @@ int main() {
 }
 
 void PostRegisterUser(const std::shared_ptr<restbed::Session> session) {
+    using namespace rapidjson;
+
     const auto req = session->get_request();
     int content_length = req->get_header("Content-Length", 0);
 
     session->fetch(content_length, [](const std::shared_ptr<restbed::Session> session, const restbed::Bytes& body) {
+        std::stringstream user_ss;
+        user_ss << body.data() << std::flush;
+
+        Document user_json;
+        user_json.Parse(user_ss.str().c_str());
+
+        if (user_json.HasParseError()) {
+             throw std::runtime_error("Error parsing user JSON");
+        }
+
         std::cout << (int)body.size() << " | " << body.data() << std::endl;
         session->close(restbed::OK, "POST Register User");
     });
 }
 
 void PostVehicle(std::shared_ptr<restbed::Session> session) {
+    using namespace rapidjson;
+
     const auto req = session->get_request();
     int content_length = req->get_header("Content-Length", 0);
 
     session->fetch(content_length, [](const std::shared_ptr<restbed::Session> session, const restbed::Bytes& body) {
-        std::cout << (int)body.size() << " | " << body.data() << std::endl;
+        //std::cout << (int)body.size() << " | " << body.data() << std::endl;
+
+        std::stringstream vehicle_ss;
+        vehicle_ss << body.data() << std::flush;
+        vehicle_ss.str().erase(std::remove(vehicle_ss.str().begin(), vehicle_ss.str().end(), '\\'), vehicle_ss.str().end());
+        std::cout << vehicle_ss.str() << std::endl;
+
+        Document vehicle_json;
+        vehicle_json.Parse(vehicle_ss.str().c_str());
+
+        if (vehicle_json.HasParseError()) {
+            throw std::runtime_error("Error parsing user JSON");
+        }
+
+        if (vehicles.find(vehicle_json["vid"].GetString()) == vehicles.end()) {
+            vehicles.emplace(vehicle_json["vid"].GetString(), default_halted);
+        }
+
         session->close( restbed::OK, "POST Vehicle", { { "Content-Length", "13" } } );
     });
 }
 
 void GetVehicle(const std::shared_ptr<restbed::Session> session) {
+    using namespace rapidjson;
+
     const auto req = session->get_request();
     int content_length = req->get_header("Content-Length", 0);
 
     session->fetch(content_length, [](const std::shared_ptr<restbed::Session> session, const restbed::Bytes& body) {
+        std::stringstream vehicle_ss;
+        vehicle_ss << body.data() << std::flush;
+
+        Document vehicle_json;
+        vehicle_json.Parse(vehicle_ss.str().c_str());
+
+        if (vehicle_json.HasParseError()) {
+            throw std::runtime_error("Error parsing user JSON");
+        }
+
         std::cout << (int)body.size() << " | " << body.data() << std::endl;
         session->close( restbed::OK, "GET Vehicle", { { "Content-Length", "13" } } );
     });
 }
 
 void UpdateVehicle(const std::shared_ptr<restbed::Session> session) {
+    using namespace rapidjson;
+
     const auto req = session->get_request();
     int content_length = req->get_header("Content-Length", 0);
 
     session->fetch(content_length, [](const std::shared_ptr<restbed::Session> session, const restbed::Bytes& body) {
-        std::cout << (int)body.size() << " | " << body.data() << std::endl;
+        std::stringstream vehicle_ss;
+        vehicle_ss << body.data() << std::flush;
+
+        Document vehicle_json;
+        vehicle_json.Parse(vehicle_ss.str().c_str());
+
+        if (vehicle_json.HasParseError()) {
+            throw std::runtime_error("Error parsing user JSON");
+        }
+
         session->close( restbed::OK, "UPDATE Vehicle", { { "Content-Length", "13" } } );
     });
 }
 
 void DeleteVehicle(const std::shared_ptr<restbed::Session> session) {
+    using namespace rapidjson;
+
     const auto req = session->get_request();
     int content_length = req->get_header("Content-Length", 0);
 
     session->fetch(content_length, [](const std::shared_ptr<restbed::Session> session, const restbed::Bytes& body) {
+        std::stringstream vehicle_ss;
+        vehicle_ss << body.data() << std::flush;
+
+        Document vehicle_json;
+        vehicle_json.Parse(vehicle_ss.str().c_str());
+
+        if (vehicle_json.HasParseError()) {
+            throw std::runtime_error("Error parsing user JSON");
+        }
+
         std::cout << (int)body.size() << " | " << body.data() << std::endl;
         session->close( restbed::OK, "DELETE Vehicle", { { "Content-Length", "13" } } );
     });
