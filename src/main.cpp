e8f989519166ee9d59ae6de30eb21fa4c7daf221
Dilpreet Chana <dschana6@gmail.com>
2020-10-23 23:01:56 -0400

    POST vehicles

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,16 @@
-// TODO: Get the token from REST server to authorize vehicle actions because SQL is a hot garbage
-
 #include <algorithm>
-#include <cstdlib>
+#include <fstream>
 #include <iostream>
 #include <map>
 #include <memory>
 #include <rapidjson/document.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
 #include <restbed>
-#include <sstream>
 #include <string>
 #include <vcar.h>
+
+#define VID_JSON_FILE "vids.json"
 
 void PostRegisterUser(std::shared_ptr<restbed::Session> session);
 void PostVehicle(std::shared_ptr<restbed::Session> session);
@@ -17,6 +18,11 @@ void GetVehicle(std::shared_ptr<restbed::Session> session);
 void UpdateVehicle(std::shared_ptr<restbed::Session> session);
 void DeleteVehicle(std::shared_ptr<restbed::Session> session);
 
+std::string getStringFromJSON(rapidjson::Value const& doc);
+void parseJSONFromFile(std::string file_name, rapidjson::Document& doc);
+void saveJSONToFile(std::string file_name, rapidjson::Value& doc);
+
+rapidjson::Document vids;
 bool running = true;
 bool default_halted = true;
 std::map<std::string, vcar> vehicles;
@@ -24,6 +30,9 @@ std::map<std::string, vcar> vehicles;
 int main() {
     vehicles.emplace("test", default_halted);
     //vehicles["test"].launch();
+
+    // Load vid files JSON
+    parseJSONFromFile(VID_JSON_FILE, vids);
 
     /// REST communication setup
     auto register_user = std::make_shared<restbed::Resource>();
@@ -84,25 +93,45 @@ void PostVehicle(std::shared_ptr<restbed::Session> session) {
     int content_length = req->get_header("Content-Length", 0);
 
     session->fetch(content_length, [](const std::shared_ptr<restbed::Session> session, const restbed::Bytes& body) {
-        //std::cout << (int)body.size() << " | " << body.data() << std::endl;
-
+        // Clean incoming data JSON
         std::stringstream vehicle_ss;
         vehicle_ss << body.data() << std::flush;
-        vehicle_ss.str().erase(std::remove(vehicle_ss.str().begin(), vehicle_ss.str().end(), '\\'), vehicle_ss.str().end());
-        std::cout << vehicle_ss.str() << std::endl;
+        std::string vjson_str = vehicle_ss.str();
+        vjson_str.erase(std::remove(vjson_str.begin(), vjson_str.end(), '\\'), vjson_str.end());
 
+        // Parse data JSON
         Document vehicle_json;
-        vehicle_json.Parse(vehicle_ss.str().c_str());
+        vehicle_json.Parse(vjson_str.c_str());
 
         if (vehicle_json.HasParseError()) {
             throw std::runtime_error("Error parsing user JSON");
         }
 
-        if (vehicles.find(vehicle_json["vid"].GetString()) == vehicles.end()) {
-            vehicles.emplace(vehicle_json["vid"].GetString(), default_halted);
+        std::string email = vehicle_json["email"].GetString();
+        std::string vid = vehicle_json["vid"].GetString();
+
+        // New email
+        if (!vids.HasMember(email.c_str())) {
+            Value vid_list(kArrayType);
+            vids.AddMember(Value(email.c_str(), vids.GetAllocator()), vid_list, vids.GetAllocator());
         }
 
-        session->close( restbed::OK, "POST Vehicle", { { "Content-Length", "13" } } );
+        if (vids[email.c_str()].GetArray().Size() >= 5) {
+            session->close(restbed::BAD_REQUEST, "Vehicle limit reached");
+            return;
+        }
+
+        // Create vid entry
+        vids[email.c_str()].GetArray().PushBack(Value(vid.c_str(), vids.GetAllocator()), vids.GetAllocator());
+
+        // Create car
+        if (vehicles.find(vid) == vehicles.end()) {
+            vehicles.emplace(vid, default_halted);
+        }
+
+        saveJSONToFile(VID_JSON_FILE, vids);
+
+        session->close( restbed::CREATED, "New vehicle created");
     });
 }
 
@@ -169,4 +198,35 @@ void DeleteVehicle(const std::shared_ptr<restbed::Session> session) {
         std::cout << (int)body.size() << " | " << body.data() << std::endl;
         session->close( restbed::OK, "DELETE Vehicle", { { "Content-Length", "13" } } );
     });
+}
+
+std::string getStringFromJSON(rapidjson::Value const& doc) {
+    rapidjson::StringBuffer buffer;
+    buffer.Clear();
+
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    doc.Accept(writer);
+
+    return std::string(buffer.GetString());
+}
+
+void parseJSONFromFile(const std::string file_name, rapidjson::Document& doc) {
+    // Load vid file
+    std::ifstream fs(file_name);
+    fs.seekg(0, fs.end);
+    int file_length = fs.tellg();
+    fs.seekg(0, fs.beg);
+
+    char* file_content = new char[file_length];
+    fs.read(file_content, file_length);
+
+    doc.Parse(file_content);
+
+    delete[] file_content;
+}
+
+void saveJSONToFile(const std::string file_name, rapidjson::Value& doc) {
+    std::ofstream fs(file_name, std::iostream::trunc);
+    fs << getStringFromJSON(doc);
+    fs.close();
 }
